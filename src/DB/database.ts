08273dd5d9125c1c5ff99d7f3817f43028abe11e
tanvir <tanvirislam173389@gmail.com>
2026-05-21 19:33:05 +0600

    added issues table

--- src/DB/database.ts
+++ src/DB/database.ts
@@ -19,6 +19,19 @@ export const initDB = async () => {
             )
             `)
 
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS issues(
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                description TEXT NOT NULL,
+                type VARCHAR(50) NOT NULL CHECK(type IN ('bug', 'feature_request')),
+                status VARCHAR(50) NOT NULL CHECK(status IN ('open', 'in_progress', 'resolved')),
+                reporter_id INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+                )
+                `)
+
     } catch (error) {
         console.log(error)
     }
