451e892fcbf4acca95ba65049c87fbe8b1fa28cf
tanvir <tanvirislam173389@gmail.com>
2026-05-22 12:18:32 +0600

    status updated

--- src/Modules/Issues/issues.controller.ts
+++ src/Modules/Issues/issues.controller.ts
@@ -5,7 +5,7 @@ import { issuesService } from "./issues.service";
 const createIssues = async (req: Request, res: Response) => {
     try {
         const result = await issuesService.insertIssuesInDB(req.body, req.headers.authorization as string)
-        response(res, true, 200, "Issue created successfully", result)
+        response(res, true, 201, "Issue created successfully", result)
     } catch (error) {
         errorResponse(res, false, 400, "Something went wrong", error)
     }
